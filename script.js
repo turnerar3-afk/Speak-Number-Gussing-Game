2b79e434e41633dd0d8d5d527baf049f7d3af5c7
Amaree Turner <amareeturner@macbookpro.myfiosgateway.com>
2025-10-01 22:44:48 -0400

    Add script.js

--- script.js
+++ script.js
@@ -19,8 +19,17 @@ recognition.start();
 // Capture user speak
 function onSpeak(event) {
   const msg = event.results[0][0].transcript;  // You can log the event to view the structure of the data
-  console.log(msg);
+    writeMessage(msg); 
 }
 
 // Speak result
 recognition.addEventListener('result', onSpeak);
+
+// See in DOM what user speaks
+function writeMessage(msg) {
+    msgEl.innerHTML =
+        `<div>You said: </div>
+        <span class="box">${msg}</span>
+        `;   
+
+}
